e06b2594b578d4701b8c477cd7dbd8973e2157b5
jschasse <jaredchasse4@msn.com>
2025-05-09 11:58:53 -0400

    added sql databases and sqlc go functionality. users are now stored in databases and can be registered to the database for the ability to login.

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
+import _ "github.com/lib/pq"
+
 import(
 	"fmt"
 	"github.com/jschasse/blogaggregator/internal/config"
+	"github.com/jschasse/blogaggregator/internal/database"
+	"database/sql"
 	"os"
+	"context"
+	"github.com/google/uuid"
+	"time"
 )
 
 type state struct {
+	db *database.Queries
 	configPtr *config.Config 
 }
 
@@ -36,6 +44,12 @@ func main() {
 	s = &state{}
 	s.configPtr = cPtr
 
+	db, err := sql.Open("postgres", s.configPtr.Db_url)
+
+	dbQueries := database.New(db)
+
+	s.db = dbQueries
+
 	if len(os.Args) < 2 {
 		fmt.Printf("Need a command name\n")
 		os.Exit(1)
@@ -48,6 +62,7 @@ func main() {
 
 	
 	myCommands.list["login"] = handlerLogin
+	myCommands.list["register"] = handlerRegister
 
 	err = myCommands.run(s, co)
 	if err != nil {
@@ -62,12 +77,50 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("the login handler expects a single argument, the username\n")
 	}
 
-	err := s.configPtr.SetUser(cmd.arguments[0])
+	_, err := s.db.GetUserByName(context.Background(), cmd.arguments[0])
+	if err != nil {
+		return fmt.Errorf("User with name %s does not exist exists\n", cmd.arguments[0])
+	}
+
+	err = s.configPtr.SetUser(cmd.arguments[0])
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("The user has been set\n")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if cmd.arguments[0] == "" || len(cmd.arguments[0]) == 0 {
+		return fmt.Errorf("Register command needs a name\n")
+	}
+
+	_, err := s.db.GetUserByName(context.Background(), cmd.arguments[0])
+	if err == nil {
+		return fmt.Errorf("User with name %s already exists\n", cmd.arguments[0])
+	} else if err != sql.ErrNoRows {
+		return fmt.Errorf("Error checking if user exists")
+	}
+
+	params := database.CreateUserParams{
+		ID: 	   uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:	   cmd.arguments[0],
+	}
+
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return err
+	}
+
+	err = s.configPtr.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s", user)
+
 	return nil
 }
 
